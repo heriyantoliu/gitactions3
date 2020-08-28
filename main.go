59a0673fc2d2a65c4e52ad547556089d36dee997
heriyanto <heriyanto.liu@sidomuncul.co.id>
2020-08-28 13:23:26 +0700

    remove B001

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ type student struct {
 var data = []student{
 	student{"E001", "ethan", 21},
 	student{"W001", "wick", 22},
-	student{"B001", "bourne", 23},
+	// student{"B001", "bourne", 23},
 	student{"B002", "bond", 23},
 }
 
